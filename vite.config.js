c97b8e6115df65ebaf4d1903ee60e08579f2b48a
hoang01n <116301378+hoang01n@users.noreply.github.com>
2024-08-07 01:16:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3001,
-  },
+  // server: {
+  //   port: 3001,
+  // },
   resolve: {
     extensions: [".js", ".jsx"],
     alias: {
